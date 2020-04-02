f8729db9341bf0432dffde9767289414210d8058
keven <yuezmkeven@gmail.com>
2020-04-02 14:33:19 +0800

    fix: controller测试用例覆盖，修改package.json

--- public/template/controller.ts
+++ public/template/controller.ts
@@ -1,8 +1,8 @@
 import { Controller } from '@nestjs/common';
-import { TestService } from './service/test.service';
+import { <%= nameHump %>Service } from './service/<%= name %>.service';
 
-@Controller('/v1/test')
-export class TestController {
-  constructor(private readonly testService: TestService) {
+@Controller('/v1/<%= name %>')
+export class <%= nameHump %>Controller {
+  constructor(private readonly <%= name %>Service: <%= nameHump %>Service) {
   }
 }
